1345cba01a68b5b013e136bad2fd69ec5c1de83a
Shauray <shauray9@gmail.com>
2021-08-09 23:28:01 +0530

    augmenting and finding out the best possible augmentations

---
+++ data-ai/README.md
@@ -0,0 +1,4 @@
+# Augmentations 
+
+
+Updating 
--- data-ai/augmentation.py
+++ data-ai/augmentation.py
@@ -1,8 +1,20 @@
+## -------------------- AUTO AUGMENT -------------------- ##
+
 import albumentation 
 import torch
 import torch.nn as nn
 
 import torchvision
+
+class RomanPolicy(object):
+    ## -------------------- the probability of applying the operation -------------------- ##
+    ## -------------------- the magnitude of the operation -------------------- ##
+
+    def __init__(self, fillcolor=(128,128,128)):
+        self.policies[
+        SubPolicy(.4, "posterize", 8, .6, "rotate", 9, fillcolor),
+        ]
+
 
 if __name__ == "__main__":
     print("some augmentations to improve the dataset !")
